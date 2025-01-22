21daf5123637800093eb9871c134ec3184d3782a
RJ-06 <rahulj0506@gmail.com>
2025-01-21 21:00:15 -0800

    push

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,21 +1,30 @@
 using NUnit.Framework;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     ArrayList playerList = new ArrayList();
+    GameObject[] players;
+
+    [SerializeField] int playerCount;
+    PlayerInputManager playerInputManager;
+
+    int chosenPlayer = 0;
+
+    bool joined = false;
 
     void Start()
     {
+        /*for (int i = 0; i < playerCount; i++) { playerInputManager.JoinPlayer(); }
         var objects = FindObjectsByType<PlayerVals>(FindObjectsSortMode.None);
         foreach (var p in objects) { playerList.Add(p.gameObject); }
-    }
+        chosenPlayer = Random.Range(0, playerList.Count);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PlayerVals player = ((GameObject)playerList[chosenPlayer]).GetComponent<PlayerVals>();
+        player.setHasPotato(true);*/
+
     }
 }
