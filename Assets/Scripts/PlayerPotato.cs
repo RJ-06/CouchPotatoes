4b9360ad5249ebb5ba552a2101b46a44b3b25eda
Joshua Villanova <joshuacvillanova@gmail.com>
2025-01-30 10:30:27 -0800

    add double jump

--- Assets/Scripts/PlayerPotato.cs
+++ Assets/Scripts/PlayerPotato.cs
@@ -23,6 +23,7 @@ public class PlayerPotato : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        onGetPotato();
     }
 
     // Update is called once per frame
