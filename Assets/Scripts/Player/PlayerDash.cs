d3fc62b735f93c6f42ede1627492426a47e5b633
Jason Lin <lin1jason8@outlook.com>
2025-02-08 18:35:31 -0800

    Fixed dashing mechanism
    - Added `isDashing` flag to track dash state.
    - Added the float variable `dashSpeedMultiplier` to modify movement speed while dashing.
    - Updated `OnMovement()` to apply speed dynamically using `ApplyMovementSpeed()`.
    - Implemented `setDashing(bool dashing)` to enable or disable dash mode.
    - Added `ForceSpeedUpdate()` to allow external scripts to immediately update movement speed.

--- Assets/Scripts/Player/PlayerDash.cs
+++ Assets/Scripts/Player/PlayerDash.cs
@@ -6,11 +6,14 @@ public class PlayerDash : MonoBehaviour
     [SerializeField] float dashCooldownTimer;
     [SerializeField] float dashTimer;
     private bool dashInProgress = false;
+    private float originalMovesSpeed;
+    private float dashSpeedMultiplier = 2.0f;
 
     private PlayerVals pv;
     private PlayerInput playerInput;
     private Rigidbody2D rb;
     private PlayerMovement playerMovement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +23,7 @@ public class PlayerDash : MonoBehaviour
         dashCooldownTimer = pv.getDashCooldown();
         dashTimer = pv.getDashTime();
         playerMovement = GetComponent<PlayerMovement>();
+        originalMovesSpeed = pv.getMoveSpeed();
     }
     
     private void FixedUpdate()
@@ -27,10 +31,13 @@ public class PlayerDash : MonoBehaviour
         // Debug.Log(rb.linearVelocity.magnitude);
         dashCooldownTimer -= Time.fixedDeltaTime;
 
-        if (dashInProgress && Mathf.Abs(dashTimer) >= 0.0001) {
+        if (dashInProgress && Mathf.Abs(dashTimer) >= 0.0001) {     // Dashing is on progress
             dashTimer -= Time.fixedDeltaTime;
-        } else if (dashInProgress) {
-            rb.AddForce(-1 * playerMovement.getMoveDir() * pv.getDashSpeed());
+        } else if (dashInProgress) {    // Dashing has end as the timer <= 0
+
+            // rb.AddForce(-1 * playerMovement.getMoveDir() * pv.getDashSpeed());  // Apply opposite force to stop the dashing
+
+            playerMovement.setDashing(false);
             dashInProgress = false;
             dashTimer = pv.getDashTime();
         }
@@ -38,10 +45,13 @@ public class PlayerDash : MonoBehaviour
 
     private void OnDash() 
     {
-        if (dashCooldownTimer > 0) return;
+        if (dashCooldownTimer > 0) return;      // > 0 meaning in cooldown, <= 0 meaning can be used 
+ 
         dashCooldownTimer = pv.getDashCooldown();
-
-        rb.AddForce(playerMovement.getMoveDir() * pv.getDashSpeed());
         dashInProgress = true;
+
+        // rb.AddForce(playerMovement.getMoveDir() * pv.getDashSpeed());
+        playerMovement.setDashing(true);
+        playerMovement.ForceSpeedUpdate();
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,6 +18,10 @@ public class PlayerMovement : MonoBehaviour
     
     private bool canMove = true;
     private bool pushed = false;
+
+    private bool isDashing = false;
+    private float dashSpeedMultiplier = 2.0f;
+
     
     [SerializeField] PlayerPotato player;
     
@@ -46,13 +50,23 @@ public class PlayerMovement : MonoBehaviour
 
             rb.linearVelocity = moveDir * pv.getMoveSpeed();
             ClampSpeed();
+
+            ApplyMovementSpeed();
         }
     }
 
-    private void OnInteract()
+
+    private void ApplyMovementSpeed()   // Set current speed based on is dashing is triggered or not
     {
-        Debug.Log("Interacted");
-        
+        float speed;
+
+        if (isDashing)
+            speed = pv.getMoveSpeed() * dashSpeedMultiplier;
+        else
+            speed = pv.getMoveSpeed();
+
+        rb.linearVelocity = moveDir * speed;
+        ClampSpeed();
     }
 
     private void ClampSpeed() 
@@ -61,6 +75,12 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.linearVelocity = rb.linearVelocity.normalized * pv.getMaxSpeed();
         }
+    }
+
+    private void OnInteract()
+    {
+        Debug.Log("Interacted");
+        
     }
 
     private void OnStart()
@@ -78,8 +98,16 @@ public class PlayerMovement : MonoBehaviour
         pushed = false;
     }
 
+
     public Vector2 getMoveDir() => moveDir;
     public void SetCanMove(bool canMove) => this.canMove = canMove;
+
+    public void setDashing(bool dashing) {
+        isDashing = dashing; 
+        ApplyMovementSpeed();
+    }
+
+    public void ForceSpeedUpdate() => ApplyMovementSpeed();     // Allow external script to force an immediate speed update.
     
 
 }
