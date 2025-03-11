670ed2e2edbaa4536da2b0f694c8e01c2c5f25de
Joshua Villanova <joshuacvillanova@gmail.com>
2025-03-11 02:58:42 -0700

    update grassy tilemap

--- Assets/Scripts/UI/In Game/Healthbar.cs
+++ Assets/Scripts/UI/In Game/Healthbar.cs
@@ -7,11 +7,14 @@ public class Healthbar : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 0.9f, 0);
 
-
+    // NOTE TO JASON: I have commented out the problematic lines because they are causing compiler errors!
+    // To access the getHealth function, you need to go through the player GameObject's PlayerVals component
+    // i.e. for some player Player do:
+    // Player.GetComponent<PlayerVals>()
 
     private void Start() {
-        slider.maxValue = player.getHealth();
-        slider.value = player.getHealth();
+        /*slider.maxValue = player.getHealth();
+        slider.value = player.getHealth();*/
 
     }
     void LateUpdate () 
@@ -20,7 +23,7 @@ public class Healthbar : MonoBehaviour
 
         transform.position = player.transform.position + offset;
 
-        slider.value = player.getHealth();
+        //slider.value = player.getHealth();
     }
 
 }
