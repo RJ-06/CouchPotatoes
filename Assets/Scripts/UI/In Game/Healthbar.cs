689b34143d2d22ba593b4498d5581bad6902d22c
Jason Lin <lin1jason8@outlook.com>
2025-03-11 22:58:50 -0700

    Added health bar display on the Player object
    - The health bar only display when the player's health changes.
    - It will display for about a second and fade away

--- Assets/Scripts/UI/In Game/Healthbar.cs
+++ Assets/Scripts/UI/In Game/Healthbar.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Healthbar : MonoBehaviour
 {
@@ -7,23 +8,70 @@ public class Healthbar : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 0.9f, 0);
 
-    // NOTE TO JASON: I have commented out the problematic lines because they are causing compiler errors!
-    // To access the getHealth function, you need to go through the player GameObject's PlayerVals component
-    // i.e. for some player Player do:
-    // Player.GetComponent<PlayerVals>()
+    [SerializeField] private float displayTime = 0.8f;
+    [SerializeField] private float fadeSpeed = 1f;
+    private CanvasGroup canvasGroup;
+    private float prevHealth;
+    private Coroutine fadeCoroutine;
 
-    private void Start() {
-        /*slider.maxValue = player.getHealth();
-        slider.value = player.getHealth();*/
+    private void Start()
+    {
+        slider.maxValue = player.GetComponent<PlayerVals>().getHealth();
+        slider.value = player.GetComponent<PlayerVals>().getHealth();
+        prevHealth = slider.value;
 
+        // Get or add CanvasGroup
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+
+        // Hide initially
+        canvasGroup.alpha = 0f;
     }
-    void LateUpdate () 
+
+    void LateUpdate()
     {
         if (player == null) return;
 
         transform.position = player.transform.position + offset;
 
-        //slider.value = player.getHealth();
+        slider.value = player.GetComponent<PlayerVals>().getHealth();
+
+        // If health changed
+        if (slider.value != prevHealth)
+        {
+            Debug.Log($"Health changed! Previous: {prevHealth}, Current: {slider.value}");
+            ShowHealthBar();
+        }
+
+        prevHealth = slider.value;
     }
 
+    private void ShowHealthBar()
+    {
+        if (fadeCoroutine != null)
+        {
+            StopCoroutine(fadeCoroutine);
+        }
+
+        // Show the health bar
+        canvasGroup.alpha = 1f;
+
+        // Start new fade
+        fadeCoroutine = StartCoroutine(FadeOutHealthBar());
+    }
+
+    private IEnumerator FadeOutHealthBar()
+    {
+
+        yield return new WaitForSeconds(displayTime);
+
+        Debug.Log("Starting fade out");
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime * fadeSpeed;
+            yield return null;
+        }
+
+    }
 }
