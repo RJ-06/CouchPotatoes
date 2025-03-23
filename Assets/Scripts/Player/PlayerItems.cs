e2986ad931626bc874b295188adacb3faf9f7b60
Heborine <36609533+Heborine@users.noreply.github.com>
2025-03-22 18:45:00 -0700

    directional weak attack

--- Assets/Scripts/Player/PlayerItems.cs
+++ Assets/Scripts/Player/PlayerItems.cs
@@ -17,12 +17,7 @@ public class PlayerItems: MonoBehaviour
     [SerializeField] float weakCooldown;
     [SerializeField] float weakCooldownTimer;
     private bool weakUsed = false;
-    // [SerializeField] float weakRange = 2f;
-    // [SerializeField] int weakDamage = 1;
-    // [SerializeField] float knockbackForceStrength = 0.5f;
-    // [SerializeField] float arcAngle = 33f;
-    // [SerializeField] int numRays = 5;
-    // private LineRenderer lineRenderer;
+    private Vector2 shootDir;
 
     // Shockwave attack
     [SerializeField] float shockwaveCooldown;
@@ -33,14 +28,6 @@ public class PlayerItems: MonoBehaviour
     {
         pv = GetComponent<PlayerVals>();
         shockwaveCooldown = pv.getAttackCooldown();
-
-        // lineRenderer = gameObject.AddComponent<LineRenderer>();
-        // lineRenderer.material = new Material(Shader.Find("Sprites/Default")); 
-        // lineRenderer.startWidth = 0.05f;  
-        // lineRenderer.endWidth = 0.05f;
-        // lineRenderer.positionCount = 2;  
-        // lineRenderer.startColor = Color.red;  
-        // lineRenderer.endColor = Color.red;
     }
 
     void FixedUpdate()
@@ -88,34 +75,23 @@ public class PlayerItems: MonoBehaviour
     
     private void WeakAttack()
     {
-        // Debug.Log("Weak Attack");
-        Vector3 attackPos = new Vector3(
-            transform.position.x + transform.forward.x, 
-            transform.position.y + 1f, 
-            transform.position.z + transform.forward.z
-        );        
-        // Debug.Log(attackPos);
-        // Debug.Log(transform.position);
-        GameObject weakAttack = Instantiate(weakAttackPrefab, attackPos, Quaternion.identity, transform.gameObject.transform);
-        Destroy(weakAttack, 1f);
-        // Vector3 dirAtk = transform.forward;
-        // // get the angle between each ray
-        // float arcStep = arcAngle / (numRays - 1);
-        // for(int i = 0; i < numRays; i++)
-        // {
-        //     // direction of each ray within the overall arc
-        //     float currAngle = (-arcAngle / 2) + (i * arcStep);
-        //     Vector3 dir = Quaternion.Euler(0, currAngle, 0) * dirAtk;
-        //     Vector3 endOfRay = transform.position + dir * weakRange;
-        //     lineRenderer.SetPosition(0, transform.position);
-        //     lineRenderer.SetPosition(1, endOfRay);
-        //     RaycastHit hit;
-        //     if(Physics.Raycast(transform.position, dir, out hit, weakRange))
-        //     {
-        //         hit.transform.GetComponent<PlayerVals>().IncrementHealth(-weakDamage);
-        //         hit.transform.GetComponent<Rigidbody>().AddForce(dir * knockbackForceStrength, ForceMode.Impulse);
-        //     }
+        if (shootDir != Vector2.zero){
+            Vector2 normalizedShootDir = shootDir.normalized;
+            Vector3 attackPos = new Vector3(
+                transform.position.x + normalizedShootDir.x, 
+                transform.position.y + normalizedShootDir.y, 
+                transform.position.z 
+                );        
+            Quaternion rot = Quaternion.LookRotation(Vector3.forward, shootDir);
+            GameObject weakAttack = Instantiate(weakAttackPrefab, attackPos, rot, transform.gameObject.transform);
 
-        // }
+            Destroy(weakAttack, 1f);
+        }
+        
+    }
+
+    private void OnAim(InputValue val) // Aim the potato with the right joystick on controller
+    {
+        shootDir = val.Get<Vector2>();
     }
 }
