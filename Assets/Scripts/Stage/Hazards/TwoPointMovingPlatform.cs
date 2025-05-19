4d299da219418fd7f42d19ae94c1c177c7a74e97
Joshua Villanova <joshuacvillanova@gmail.com>
2025-05-18 21:01:29 -0700

    fix player movement on platform issue
    Players previously would slide over a platform when it was moving, and then be "dragged" along by the collider. Code is now implemented to prevent this by adjusting the player's position along with the platform's movement.

--- Assets/Scripts/Stage/Hazards/TwoPointMovingPlatform.cs
+++ Assets/Scripts/Stage/Hazards/TwoPointMovingPlatform.cs
@@ -9,17 +9,19 @@ public class TwoPointMovement : MonoBehaviour
     [SerializeField] float moveTime, idleTime;
     [SerializeField] Vector2 firstPoint, secondPoint;
     [SerializeField] TilemapCollider2D colliderBounds;
-    [SerializeField] List<GameObject> players;
+    private List<PlayerMovement> players;
     private bool initialized = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        players = new List<PlayerMovement>();
+        // Check for players inside the platform on scene load (might not be necessary)
         List<GameObject> allPlayers = transform.parent.gameObject.GetComponent<GameManager>().GetPlayers();
         foreach (GameObject player in allPlayers)
         {
-            if (PlayerInsidePlatform(player))
+            if (PlayerInsidePlatform(player) && !players.Contains(player.GetComponent<PlayerMovement>()))
             {
+                players.Add(player.GetComponent<PlayerMovement>());
                 Debug.Log("A player is initially inside a platform");
             }
         }
@@ -28,13 +30,16 @@ public class TwoPointMovement : MonoBehaviour
 
     void Update()
     {
+        // Check for players inside the platform as first game starts
         if (!initialized && transform.parent.gameObject.GetComponent<GameManager>().GetFirstGameStarted())
         {
             List<GameObject> allPlayers = transform.parent.gameObject.GetComponent<GameManager>().GetPlayers();
             foreach (GameObject player in allPlayers)
             {
-                if (PlayerInsidePlatform(player))
+                if (player != null && PlayerInsidePlatform(player)
+                && !players.Contains(player.GetComponent<PlayerMovement>()))
                 {
+                    players.Add(player.GetComponent<PlayerMovement>());
                     Debug.Log("A player is initially inside a platform");
                 }
             }
@@ -54,13 +59,19 @@ public class TwoPointMovement : MonoBehaviour
             while (t < 1)
             {
                 t = (Time.time - moveStartTime) / timeToMove;
-                transform.position = new Vector2(Mathf.SmoothStep(firstPoint.x, secondPoint.x, t), Mathf.SmoothStep(firstPoint.y, secondPoint.y, t));
+                Vector2 previousPos = new Vector2(transform.position.x, transform.position.y);
+                transform.position = new Vector2(Mathf.SmoothStep(firstPoint.x, secondPoint.x, t),
+                Mathf.SmoothStep(firstPoint.y, secondPoint.y, t));
+                Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
 
-                foreach (GameObject player in players)
+                foreach (PlayerMovement player in players)
                 {
-                    if (PlayerInsidePlatform(player))
+                    if (PlayerInsidePlatform(player.gameObject))
                     {
                         // Move the player relative to the platform's movement (no sliding)
+                        float newPosX = player.gameObject.transform.position.x + (currentPos.x - previousPos.x);
+                        float newPosY = player.gameObject.transform.position.y + (currentPos.y - previousPos.y);
+                        player.gameObject.transform.position = new Vector2(newPosX, newPosY);
                     }
                 }
                 yield return null;
@@ -73,21 +84,34 @@ public class TwoPointMovement : MonoBehaviour
             while (t < 1)
             {
                 t = (Time.time - moveStartTime) / timeToMove;
-                transform.position = new Vector2(Mathf.SmoothStep(secondPoint.x, firstPoint.x, t), Mathf.SmoothStep(secondPoint.y, firstPoint.y, t));
+                Vector2 previousPos = new Vector2(transform.position.x, transform.position.y);
+                transform.position = new Vector2(Mathf.SmoothStep(secondPoint.x, firstPoint.x, t),
+                Mathf.SmoothStep(secondPoint.y, firstPoint.y, t));
+                Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+
+                foreach (PlayerMovement player in players)
+                {
+                    if (PlayerInsidePlatform(player.gameObject))
+                    {
+                        // Move the player relative to the platform's movement (no sliding)
+                        float newPosX = player.gameObject.transform.position.x + (currentPos.x - previousPos.x);
+                        float newPosY = player.gameObject.transform.position.y + (currentPos.y - previousPos.y);
+                        player.gameObject.transform.position = new Vector2(newPosX, newPosY);
+                    }
+                }
                 yield return null;
             }
         }
     }
 
-    // THIS DOES NOT WORK
     void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("Something is inside the moving platform");
         if (other.CompareTag("Player"))
         {
             Debug.Log("That something is a player");
-            if (!players.Contains(other.gameObject))
-                players.Add(other.gameObject);
+            if (!players.Contains(other.gameObject.GetComponent<PlayerMovement>()))
+                players.Add(other.gameObject.GetComponent<PlayerMovement>());
         }
     }
 
@@ -106,10 +130,14 @@ public class TwoPointMovement : MonoBehaviour
         float height = player.GetComponent<SpriteRenderer>().bounds.size.y;
 
         // Check boundaries of player in comparison to the platform
-        bool containsTop = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x, player.transform.position.y + (height / 2), 0));
-        bool containsBottom = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x, player.transform.position.y - (height / 2), 0));
-        bool containsLeft = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x - (width / 2), player.transform.position.y, 0));
-        bool containsRight = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x + (width / 2), player.transform.position.y, 0));
+        bool containsTop = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x,
+        player.transform.position.y + (height / 2), 0));
+        bool containsBottom = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x,
+        player.transform.position.y - (height / 2), 0));
+        bool containsLeft = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x - (width / 2),
+        player.transform.position.y, 0));
+        bool containsRight = colliderBounds.bounds.Contains(new Vector3(player.transform.position.x + (width / 2),
+        player.transform.position.y, 0));
         if (containsTop && containsBottom && containsLeft && containsRight)
         {
             Debug.Log("A player is fully inside a moving platform");
