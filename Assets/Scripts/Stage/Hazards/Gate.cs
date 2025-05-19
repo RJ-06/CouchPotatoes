3310a4d181967e78a7ec03405bfa7836e28db1ea
Joshua Villanova <joshuacvillanova@gmail.com>
2025-05-19 01:24:18 -0700

    make moving platforms functional
    Dashing on a platform is certain death. That is (unfortunately) a bug.

---
+++ Assets/Scripts/Stage/Hazards/Gate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] TilemapCollider2D gatePreventWalkOff, gateFallable;
+
+    // IMPORTANT: Since tilemap rigidbodies are typically static, they won't collide with each other by default. For gating functionality to
+    // work, one of the tilemaps in the collision MUST have their rigidbody set to kinematic!!! For purposes of convention, add the kinematic
+    // rigidbodies to gate tilemaps on moving platforms.
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected from a gate");
+        if (collision.CompareTag("Platform Gate"))
+        {
+            Debug.Log("Collision was between two gates");
+            gatePreventWalkOff.enabled = false;
+            gateFallable.enabled = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Platform Gate"))
+        {
+            Debug.Log("Two gates exited contact");
+            gatePreventWalkOff.enabled = true;
+            gateFallable.enabled = true;
+        }
+    }
+}
