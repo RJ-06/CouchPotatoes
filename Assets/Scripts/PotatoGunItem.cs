11b4df83e0c54c2f5dd37af852583a937c7216a5
roshilel <aronmaung@gmail.com>
2025-05-28 21:45:07 -0700

    Added Potato Gun

---
+++ Assets/Scripts/PotatoGunItem.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PotatoGunItem : ItemAttributes
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override bool AffectedByFire()
+    {
+        return true;
+    }
+}
